31cae1e96607bef09abba3859d6a8eec03b98dca
Satya Pavan <pavan.simham@gmail.com>
2018-11-14 01:37:54 +0530

    working copy. todo - add multi threading

--- scrape_sb_gallary.py
+++ scrape_sb_gallary.py
@@ -2,11 +2,11 @@ import urllib.request
 import os
 from bs4 import BeautifulSoup
             
-def save_image(image_link):
+def save_image(celeb_name, image_link):
             print("Entering into save_image:", image_link )
             c = image_link.split('/')[-1]
             s=c
-            s = os.path.join(path, s)
+            s = os.path.join(path, celeb_name, s)
             print("Saving {0} to location : {1}".format(c,s))
             output = open(s,'wb')
             resource_img = urllib.request.urlopen(image_link)
@@ -14,12 +14,12 @@ def save_image(image_link):
             output.write(data)
             output.close()
 
-def download_celeb(url):
+def download_celeb(celeb_name, url):
     print("Entering into download_celeb:",  url)
     page_no=1
     url = url + '?page='
     print(url + str(page_no))
-    while (page_no <= MAX_NUMBER_OF_PAGES):
+    while (page_no <= PAGE_DEPTH):
         resource_celeb = urllib.request.urlopen(url + str(page_no))
         celeb_soup = BeautifulSoup(resource_celeb, 'html.parser')
         #print(celeb_soup)
@@ -27,7 +27,7 @@ def download_celeb(url):
             #print(link)
             image_link = link.find('img').get('src')
             print("", image_link)
-            save_image(image_link)
+            save_image(celeb_name, image_link)
 
             width = link.find('img').get('width')
             height = link.find('img').get('height')
@@ -40,29 +40,30 @@ def downloader(soup):
     for link in soup.select('div .imgdiv1 > a'):
         celeb_href=link.get('href')
         celeb_name=link.find('img').get('alt')
+        celeb_name=(celeb_name.replace(' ', '-')).lower()        
         print(celeb_href, celeb_name);
-        download_celeb(base_url + celeb_href);
+        os.mkdir(os.path.join(path, celeb_name))
+        download_celeb(celeb_name, base_url + celeb_href);
 
 
 #########################################################################
-path = "H:\\coding\\git\\scratch-pad\\"
+path = "H:\\coding\\git\\scratch-pad\\pics\\"
 
 # specify the url
-MAX_NUMBER_OF_PAGES=2
+PAGE_DEPTH=15
+
 base_url = 'http://www.santabanta.com'
-quote_page = 'http://www.santabanta.com/images/parent/2/'
+gallary_page = 'http://www.santabanta.com/images/parent/2/?page='
 #quote_page = 'http://www.santabanta.com/photos/birds/2112292.htm'
 
-# query the website and return the html to the variable 'page’
-page = urllib.request.urlopen(quote_page)
+page_no=1
+url = gallary_page + str(page_no)
+print(url)
+while (page_no <= 5):    
+    page = urllib.request.urlopen(url)
 
-# parse the html using beautiful soup and store in variable `soup`
-soup = BeautifulSoup(page, 'html.parser')
+    soup = BeautifulSoup(page, 'html.parser')
 
+    downloader(soup)
 
-# Take out the <div> of name and get its value
-#name_box = soup.find('h1', attrs={'class': 'name'})
-
-#name = name_box.text.strip() # strip() is used to remove starting and trailing print name
-
-downloader(soup)
+    page_no = page_no + 1
